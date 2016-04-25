8bc9b2ef995bc493dab0b154909b4aa9f1eb7fbb
Bartosz Szydzik <bartosz.szydzik@gmail.com>
2016-04-25 20:18:18 +0200

    Modul03 - Dodanie konwertera jednostek (dll) i projektu do testu (zawiera referencję do biblioteki). Jak narazie dobrze zrobiona tylko klasa Lenght.cs

---
+++ Modul03/Converter/Lenght.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    
+    public static class Lenght
+    {
+        public enum UNIT { METER, MILIMETER, INCH, YARD, NAUTICALMILE, LANDMILE, KILOMETER }
+        public static double Convert(UNIT j1, double value, UNIT j2)
+        {
+            return GetLength(GetMetersFromOtherUnit(j1, value), j2);
+        }
+
+        public static double GetMetersFromOtherUnit(UNIT j, double value)
+        {
+            switch (j)
+            {
+                case UNIT.METER:
+                    return value;
+                case UNIT.MILIMETER:
+                    return value / 1000;
+                case UNIT.INCH:
+                    return value * 0.0254;
+                case UNIT.YARD:
+                    return value * 0.9144;
+                case UNIT.NAUTICALMILE:
+                    return value * 1852;
+                case UNIT.LANDMILE:
+                    return value * 1609.344;
+                case UNIT.KILOMETER:
+                    return value * 1000;
+                default:
+                    throw new ArgumentException("Nie rozpoznano jednostki");
+            }
+        }
+
+        public static double GetLength(double metr, UNIT j)
+        {
+            switch (j)
+            {
+                case UNIT.METER:
+                    return metr;
+                case UNIT.MILIMETER:
+                    return metr * 1000;
+                case UNIT.INCH:
+                    return metr / 0.0254;
+                case UNIT.YARD:
+                    return metr / 0.9144;
+                case UNIT.NAUTICALMILE:
+                    return metr / 1852;
+                case UNIT.LANDMILE:
+                    return metr / 1609.344;
+                case UNIT.KILOMETER:
+                    return metr / 1000;
+                default:
+                    throw new ArgumentException("Nie rozpoznano jednostki");
+            }
+        }
+    }
+}
---
+++ Modul03/ConverterTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Converter;
+
+namespace ConverterTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double value = Converter.Lenght.Convert(Lenght.UNIT.KILOMETER, 50, Lenght.UNIT.METER);
+            Console.WriteLine("Wynik = {0}", value);
+            Console.ReadKey();
+        }
+    }
+}
