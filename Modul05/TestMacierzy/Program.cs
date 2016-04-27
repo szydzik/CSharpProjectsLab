854ceb41a7d9bb85b1ca8f2492797d0a97faa5b8
Bartosz Szydzik <bartosz.szydzik@gmail.com>
2016-04-27 22:05:34 +0200

    Modul05 - TestMacierzy - Update - gotowe

--- Modul05/TestMacierzy/Program.cs
+++ Modul05/TestMacierzy/Program.cs
@@ -27,11 +27,53 @@ namespace TestMacierzy
             }
             Console.Write("]");
         }
+
+        private static Random generator = new Random();
+
+        public static void InicjalizujMacierz(this Macierz a)
+        {
+            for (int i = 1; i <= a.LiczbaWierszy; i++)
+            {
+                for (int j = 1; j <= a.LiczbaKolumn; j++)
+                {
+                    a[i, j] = generator.Next(0, 11);
+                }
+            }
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            Macierz a = new Macierz(3, 2), b = new Macierz(3, 2);
+            a.InicjalizujMacierz();
+            Console.WriteLine("Macierz A: ");
+            a.WypiszMacierz();
+            Console.WriteLine("\nMacierz B: ");
+            b.InicjalizujMacierz();
+            b.WypiszMacierz();
+
+            Console.WriteLine("\nMacierz A + B: ");
+            (a + b).WypiszMacierz();
+
+            Console.WriteLine("\nMacierz A - B: ");
+            (a - b).WypiszMacierz();
+
+            Console.WriteLine("\nMacierz 2 * A: ");
+            (2 * a).WypiszMacierz();
+
+            Macierz c = new Macierz(2, 1);
+            c.InicjalizujMacierz();
+            Console.WriteLine("\nMacierz C: ");
+            c.WypiszMacierz();
+            Console.WriteLine("\nMacierz A * C: ");
+            (a * c).WypiszMacierz();
+
+            Macierz d = (Macierz)33;
+            Console.WriteLine("\nMacierz D: ");
+            d.WypiszMacierz();
+
+            Console.ReadKey();
 
         }
     }
