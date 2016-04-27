77cc5757bafc44255f65ef7680827d8565a5a105
Bartosz Szydzik <bartosz.szydzik@gmail.com>
2016-04-27 22:49:03 +0200

    Modul05 - Zbiory, TestZbiory - skończone

--- Modul05/TestZbiory/Program.cs
+++ Modul05/TestZbiory/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zbiory;
 
 namespace TestZbiory
 {
@@ -10,6 +11,55 @@ namespace TestZbiory
     {
         static void Main(string[] args)
         {
+            ZbiorNapisow a = new ZbiorNapisow();
+            a.DodajElement("kot");
+            a.DodajElement("pies");
+            a.DodajElement("koza");
+
+            ZbiorNapisow b = new ZbiorNapisow();
+            b.DodajElement("krowa");
+            b.DodajElement("owca");
+            b.DodajElement("koza");
+
+            Console.Write("Zbior A:={ ");
+            for (int i = 0; i < a.MocZbioru; i++)
+            {
+                Console.Write(" {0},", a[i]);
+            }
+            Console.WriteLine(" }");
+
+            Console.Write("Zbior B:={ ");
+            for (int i = 0; i < b.MocZbioru; i++)
+            {
+                Console.Write(" {0},", b[i]);
+            }
+            Console.WriteLine(" }");
+
+            ZbiorNapisow c = a + b;
+            Console.Write("Zbior A+B:={ ");
+            for (int i = 0; i < c.MocZbioru; i++)
+            {
+                Console.Write(" {0},", c[i]);
+            }
+            Console.WriteLine(" }");
+
+            c = a - b;
+            Console.Write("Zbior A-B:={ ");
+            for (int i = 0; i < c.MocZbioru; i++)
+            {
+                Console.Write(" {0},", c[i]);
+            }
+            Console.WriteLine(" }");
+
+            c = a * b;
+            Console.Write("Zbior A*B:={ ");
+            for (int i = 0; i < c.MocZbioru; i++)
+            {
+                Console.Write(" {0},", c[i]);
+            }
+            Console.WriteLine(" }");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Modul05/Zbiory/ZbiorNapisow.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zbiory
+{
+    public class ZbiorNapisow
+    {
+        private List<string> napisy = new List<string>();
+        public void DodajElement(string s)
+        {
+            if (!napisy.Contains(s)) napisy.Add(s);
+        }
+
+        public bool CzyElementNalezy(string element)
+        {
+            return napisy.Contains(element);
+        }
+
+        public string this[int indeks]
+        {
+            get { return napisy[indeks]; }
+        }
+
+        public int MocZbioru
+        {
+            get { return napisy.Count; }
+        }
+
+        public static ZbiorNapisow operator +(ZbiorNapisow a, ZbiorNapisow b)
+        {
+            ZbiorNapisow c = new ZbiorNapisow();
+            foreach (string s in a.napisy)
+            {
+                c.DodajElement(s);
+            }
+            foreach (string s in b.napisy)
+            {
+                c.DodajElement(s);
+            }
+            return c;
+        }
+
+        public static ZbiorNapisow operator -(ZbiorNapisow a, ZbiorNapisow b)
+        {
+            ZbiorNapisow c = new ZbiorNapisow();
+            foreach (string s in a.napisy)
+            {
+                if (!b.CzyElementNalezy(s))
+                {
+                    c.DodajElement(s);
+                }
+            }
+            return c;
+        }
+
+        public static ZbiorNapisow operator *(ZbiorNapisow a, ZbiorNapisow b)
+        {
+            ZbiorNapisow c = new ZbiorNapisow();
+            foreach (string s in a.napisy)
+            {
+                if (b.CzyElementNalezy(s))
+                {
+                    c.DodajElement(s);
+                }
+            }
+            return c;
+        }
+
+
+    }
+}
