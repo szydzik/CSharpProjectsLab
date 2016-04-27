8cf867f14665280f9c64a36168b8ffdb29889d56
Bartosz Szydzik <bartosz.szydzik@gmail.com>
2016-04-27 21:17:01 +0200

    Modul05 - TestMacierzy - Update

--- Modul05/TestMacierzy/Program.cs
+++ Modul05/TestMacierzy/Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Macierze;
 
 namespace TestMacierzy
 {
+    static class MacierzDlaKonsoli
+    {
+        public static void WypiszMacierz(this Macierz a)
+        {
+            Console.Write("[ ");
+            for (int i = 1; i <= a.LiczbaWierszy; i++)
+            {
+                Console.Write("<");
+                for ( int j = 1; j<= a.LiczbaKolumn; j++)
+                {
+                    Console.Write(a[i,j]);
+                    if ( j!= a.LiczbaKolumn)
+                    {
+                        Console.Write("; ");
+                    }
+                }
+                Console.Write("> ");
+            }
+            Console.Write("]");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
