190a4fba0d0906e3415b708f59c786fd6647149d
Bartosz Sosnowski <bartosz-sosnowski@o2.pl>
2016-05-10 19:50:23 +0200

    Modul05-Zadanie 1 , pozostało tylko ograniczenie precyzji do max 20 miejsc po przecinku

---
+++ Modul05/Zadanie 1/BigNumber.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class BigNumber
+    {
+        private decimal number;
+        public static int Precision { get; set; }
+
+        public decimal Number
+        {
+            get { return number; }
+            set
+            {
+                if (value.GetType()==typeof(decimal))
+                {
+                    number = value;
+                }else
+                {
+                    Console.WriteLine("Zły typ danych");
+                }
+            }
+        }
+            
+        public BigNumber(decimal number)
+        {
+            this.number = number;
+        }
+
+        public BigNumber(double number)
+        {
+            this.number = Convert.ToDecimal(number);
+        }
+
+        public BigNumber(string number)
+        {
+            this.number = decimal.Parse(number);
+        }
+
+        public static decimal Floor(decimal value, int decimalPlaces)
+        {
+            decimal adjustment = Convert.ToDecimal(Math.Pow(10, decimalPlaces));
+            if (value >= 0) return Math.Floor(value * adjustment) / adjustment;
+            else return Math.Ceiling(value * adjustment) / adjustment;
+        }
+
+        public static BigNumber operator +(BigNumber a,BigNumber b)
+        {
+            BigNumber ret=null;
+            decimal temp = a.Number + b.Number;
+            temp = Floor(temp, Precision);
+            ret = new BigNumber(temp);
+            return ret;
+        }
+
+        public static BigNumber operator -(BigNumber a,BigNumber b)
+        {
+            BigNumber ret = null;
+            decimal temp = a.Number - b.Number;
+            temp = Floor(temp, Precision);
+            ret = new BigNumber(temp);
+            return ret;
+        }
+
+        public static BigNumber operator *(BigNumber a, BigNumber b)
+        {
+            BigNumber ret = null;
+            decimal temp = a.Number * b.Number;
+            temp = Floor(temp, Precision);
+            ret = new BigNumber(temp);
+            return ret;
+        }
+
+        public static BigNumber operator *(BigNumber a,double b)
+        {
+            BigNumber ret = null;
+            decimal temp = a.Number * Convert.ToDecimal(b);
+            temp = Floor(temp, Precision);
+            ret = new BigNumber(temp);
+            return ret;
+        }
+
+        public static BigNumber FromDouble(double number)
+        {
+            return new BigNumber(number);
+        }
+
+        public static BigNumber FromDecimal(decimal number)
+        {
+            return new BigNumber(number);
+        }
+
+        public static BigNumber FromString(string number)
+        {
+            return new BigNumber(number);
+        }
+
+        public static double ToDouble(BigNumber number)
+        {
+            return Convert.ToDouble(number.number);
+        }
+
+        public static decimal ToDecimal(BigNumber number)
+        {
+            return number.number;
+        }
+
+        public override string ToString()
+        {
+            return number.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            BigNumber a = obj as BigNumber;
+            if (a == null) return false;
+            return (number == a.Number);
+        }
+
+    }
+}
--- Modul05/Zadanie 1/Program.cs
+++ Modul05/Zadanie 1/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Zadanie_1
+namespace Zadanie1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            BigNumber n1 = new BigNumber(0.000321);
+            BigNumber n2 = new BigNumber(-0.00123m);
+            BigNumber n3 = new BigNumber(0.000321m);
+            BigNumber.Precision = 6;
+            Console.WriteLine(n1 + n2);
+            Console.ReadKey();
         }
     }
 }
