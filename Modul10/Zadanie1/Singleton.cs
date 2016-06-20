005a83bdc69d1ea87496cdcd386d76fb636a9ea6
Bartosz Sosnowski <bartosz-sosnowski@o2.pl>
2016-06-20 14:22:14 +0200

    Modul10-Dodanie Zadania1

---
+++ Modul10/Zadanie1/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    public class Singleton<T> where T : class,new()
+    {
+        private static T instance;
+
+        private Singleton()
+        {
+            return;
+        }
+        public static T getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new T();
+            }
+            return instance;
+        }
+
+    }
+}
