71e2b4ef2849a9410d0476c24d26b5e4a5c54a7a
Bartosz Szydzik <bartosz.szydzik@gmail.com>
2016-06-24 16:34:09 +0200

    Modul10 - Dodanie zadania 2. implementacja węzła

---
+++ Modul10/Zadanie2/Node.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2
+{
+    namespace BST
+    {
+        public class Node : ICloneable
+        {
+            private IComparable data;
+            private Node left, right;
+
+            #region Constructors
+            public Node() : this(null) { }
+            public Node(IComparable data) : this(data, null, null) { }
+            public Node(IComparable data, Node left, Node right)
+            {
+                this.data = data;
+                this.left = left;
+                this.right = right;
+            }
+            #endregion
+
+            #region Public Methods
+            public object Clone()
+            {
+                Node clone = new Node();
+                if (data is ICloneable)
+                    clone.Value = (IComparable)((ICloneable)data).Clone();
+                else
+                    clone.Value = data;
+
+                if (left != null)
+                    clone.Left = (Node)left.Clone();
+
+                if (right != null)
+                    clone.Right = (Node)right.Clone();
+
+                return clone;
+            }
+            #endregion
+
+            #region Public Properties
+            public IComparable Value
+            {
+                get
+                {
+                    return data;
+                }
+                set
+                {
+                    data = value;
+                }
+            }
+
+            public Node Left
+            {
+                get
+                {
+                    return left;
+                }
+                set
+                {
+                    left = value;
+                }
+            }
+
+            public Node Right
+            {
+                get
+                {
+                    return right;
+                }
+                set
+                {
+                    right = value;
+                }
+            }
+            #endregion
+
+        }
+    }
+}
