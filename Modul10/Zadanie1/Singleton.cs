7c0fe21fb241fd8723131be1e96d93ccb92121c0
Bartosz Szydzik <bartosz.szydzik@gmail.com>
2016-06-24 13:23:00 +0200

    Mod10 - Singleton dodanie wersji bezpicznej wątkowo

--- Modul10/Zadanie1/Singleton.cs
+++ Modul10/Zadanie1/Singleton.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Zadanie1
 {
-    public class Singleton<T> where T : class,new()
+    public sealed class Singleton<T> where T : class,new()
     {
         private static T instance;
 
@@ -22,6 +22,35 @@ namespace Zadanie1
                 instance = new T();
             }
             return instance;
+        }
+
+    }
+
+    public sealed class Singleton2<T> where T : class, new()
+    {
+        private static object _syncobj = new object();
+        private static volatile T _instance = null;
+
+        public Singleton2()
+        {
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncobj)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new T();
+                        }
+                    }
+                }
+                return _instance;
+            }
         }
 
     }
