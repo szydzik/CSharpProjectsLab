eb2c9b92bafafb71b4403fe74fb5e8778dd268c4
Bartosz Szydzik <bartosz.szydzik@gmail.com>
2016-06-26 17:57:07 +0200

    Modul12 - Zadanie 1 - Upadate

--- Modul10/Zadanie2/AVLTree.cs
+++ Modul10/Zadanie2/AVLTree.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AVLTree
+namespace Zadanie2
 {
     public class AVLTree<TKey, TValue> where TKey : IComparable<TKey>
     {
--- Modul10/Zadanie2/Tester.cs
+++ Modul10/Zadanie2/Tester.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AVLTree
+namespace Zadanie2
 {
     class Tester
     {
